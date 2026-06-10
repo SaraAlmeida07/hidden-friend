50b29c7a5ebb1d603886439e64635a5b3ccf6061
matheuspopolinutfpr <matheuspopolin@alunos.utfpr.edu.br>
2026-06-10 19:23:16 -0300

    feat(auth): implement Supabase Auth session management [ID21]

--- apps/web/src/environments/environment.ts
+++ apps/web/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   apiUrl: 'http://localhost:3000/api',
   googleClientId: 'INSERIR_CLIENT_ID_AQUI',
+  supabaseUrl: 'https://dveuznubdazbjayryxfa.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2ZXV6bnViZGF6YmpheXJ5eGZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxOTQwNDgsImV4cCI6MjA5MTc3MDA0OH0.SwkyjWu_AzdjDfktYa-gnkqRNbK94re8CBCWWJ2g2OQ'
 };
